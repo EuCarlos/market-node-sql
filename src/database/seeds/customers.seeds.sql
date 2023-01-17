20805506564581cf5bac90d546ecc3573a831550
Carlos Alves <carlosalves369m@hotmail.com>
2023-01-17 16:56:48 -0300

    :seedling: add seed files

---
+++ src/database/seeds/customers.seeds.sql
@@ -0,0 +1,129 @@
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(238,'A SANTOS BERNARDO & CIA LTDA - ME','altinoroberto@terra.com.br','1934584082',902,'13454355'),
+(239,'ADNELSON BALDIN & CIA LTDA ME','altinoroberto@terra.com.br','1935232220',1460,'13506669'),
+(240,'AM ORGANIZACAO FARMACEUTICA LTDA','altinoroberto@terra.com.br','1938592917',233,'13270010'),
+(241,'ANDREIVE SANTIAGO - ME','altinoroberto@terra.com.br','1932730044',48,'13030490'),
+(242,'ANTONIO MOREIRA MERCADO - EPP','altinoroberto@terra.com.br','1996020269',60,'13185211'),
+(243,'ANTONIO MOREIRA MERCADO - EPP','altinoroberto@terra.com.br','1996020269',135,'13187062'),
+(244,'CAROLINA & JEFFERSON DROGARIA LTDA -ME','altinoroberto@terra.com.br','1938761112',235,'13280000'),
+(245,'SILVEIRA & CIA LTDA-EPP','ericaalmeida@svicente.com.br','1934612715',2200,'13467000'),
+(246,'SILVEIRA & CIA LTDA-EPP','ericaalmeida@svicente.com.br','1934685878',630,'13473200');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(247,'SILVEIRA & CIA LTDA - EPP','ericaalmeida@svicente.com.br','1934595100',700,'13454214'),
+(248,'SILVEIRA & CIA LTDA - EPP','ericaalmeida@svicente.com.br','1934638000',650,'13450039'),
+(249,'SILVEIRA & CIA LTDA - EPP','ericaalmeida@svicente.com.br','1939099481',1100,'13188000'),
+(250,'SILVEIRA & CIA LTDA - EPP','ericaalmeida@svicente.com.br','1938732788',2001,'13170380'),
+(251,'COSTA & MORAES DROGARIA LTDA - ME','altinoroberto@terra.com.br','1932122569',279,'13070010'),
+(252,'DELISE & PIMENTA COM. DE PRODUTOS FARMACEUTICOS LTDA ME','altinoroberto@terra.com.br','1938281095',1744,'13175090'),
+(253,'DENTAL AZ COM. DE PROD. ODONTOLOGICOS LTDA - ME','altinoroberto@terra.com.br','1938250470',713,'13334050'),
+(254,'DENTAL-RIO COM.PROD.ODONTOLOGICOS LTDA-ME','altinoroberto@terra.com.br','1935336544',742,'13500060'),
+(255,'DERMA & DERMO COM PROD DERMA FARMAC LTDA','altinoroberto@terra.com.br','1937755000',415,'13025000'),
+(257,'DP DA SILVA DROGARIA - ME','altinoroberto@terra.com.br','19',279,'13070010');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(258,'Dr HENRIQUE ROMAN TRUFFA','altinoroberto@terra.com.br','1732325011',3290,'15015810'),
+(259,'Dr. SILVIO AMADEU NASSAR PARDO','altinoroberto@terra.com.br','1732149877',62,'15015010'),
+(264,'DROGA NOVA DE VALINHOS LTDA','altinoroberto@terra.com.br','1938696270',205,'13270100'),
+(265,'DROGA PIRES DE AMERICANA LTDA','altinoroberto@terra.com.br','1934619106',445,'13465020'),
+(266,'DROGARIA REMANSO LTDA','comprasfarmaisamanda@uol.com.br','1938652500',630,'13188160'),
+(267,'DROGARIA BARRA FUNDA LTDA','altinoroberto@terra.com.br','19',5538,'13280000'),
+(268,'DROGARIA BIASOTO LTDA - ME','drogariabiasoto@yahoo.com.br','1934621363',256,'13466310'),
+(269,'DROGARIA EUFARMA RIO PRETO LTDA - ME','altinoroberto@terra.com.br','1732312010',2634,'15025065'),
+(270,'DROGARIA EVANA LTDA - ME','altinoroberto@terra.com.br','1934684522',669,'13469425'),
+(271,'DROGARIA GABRIELLE LTDA - EPP','fvinhedo@yahoo.com.br','1938761068',253,'13280000');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(273,'DROGARIA HORTOLANDIA LTDA - EPP','comprafarmais@bol.com.br','1938655000',99,'13183000'),
+(274,'DROGARIA MINI LTDA - ME','altinoroberto@terra.com.br','1934621206',956,'13468160'),
+(275,'DROGARIA OLINDA LTDA - ME','altinoroberto@terra.com.br','1935347612',3562,'13504164'),
+(277,'DROGARIA REMANSO LTDA','comprasfarmaisremanso@uol.com.br','1938652510',831,'13184420'),
+(278,'DROGARIA AVENIDA DE VALINHOS LTDA - EPP','altinoroberto@terra.com.br','1938712528',252,'13270005'),
+(279,'DROGARIA DROGANOSSA RIO CLARO LTDA','altinoroberto@terra.com.br','1935341823',87,'13506692'),
+(280,'DROGARIA ECONOMICA DE VINHEDO LTDA - ME','altinoroberto@terra.com.br','1938763778',288,'13280000'),
+(281,'DROGARIA PRINCIPAL DE VINHEDO LTDA - EPP','altinoroberto@terra.com.br','1938763912',543,'13280000'),
+(282,'DROGARIA RENASCER VINHEDO LTDA - EPP','altinoroberto@terra.com.br','1938763778',191,'13280000'),
+(283,'DROGARIA VILA JOAO XXIII DE VINHEDO LTDA','perfumaria.vila@farmaestra.com.br','1938862846',450,'13280000');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(284,'DROGAZUL RIO CLARO LTDA - EPP','altinoroberto@terra.com.br','1935340111',105,'13500400'),
+(285,'DROGRARIA MENOR PRECO VALINHOS LTDA-ME','altinoroberto@terra.com.br','1938294397',1491,'13270000'),
+(286,'ELIANE PEREIRA BOREGGIO VALINHOS-ME','altinoroberto@terra.com.br','1938696765',85,'13272756'),
+(287,'FARLOG LOGISTICA EM MEDICAMENTOS LTDA','altinoroberto@terra.com.br','1932353010',1316,'13020432'),
+(288,'FARLOG LOGISTICA EM MEDICAMENTOS LTDA','altinoroberto@terra.com.br','1938763778',659,'13023185'),
+(289,'FARMACIA CARCAJON LTDA','altinoroberto@terra.com.br','1932417414',1361,'13073000'),
+(290,'FARMACIA E DROGARIA PASETO LTDA','altinoroberto@terra.com.br','1935242305',1807,'13500210'),
+(291,'FERRARI & FOLLE DROGARIA LTDA ME','comprasno@fazfarma.com.br','1934762050',105,'13460000'),
+(292,'FRANTINS CAMPINAS LTDA - ME','altinoroberto@terra.com.br','1933880071',1404,'13060100'),
+(293,'FUNAYAMA & AGOSTINI COM. DE PRODUTOS FARMACEUTICOS LTDA- ME','altinoroberto@terra.com.br','1938496954',658,'13276130');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(294,'IRM DA STA CASA DE MISERIC RIO CLARO','altinoroberto@terra.com.br','1935357070',297,'13500330'),
+(295,'J A MENDONCA & CIA LTDA - EPP','altinoroberto@terra.com.br','1934681588',334,'13473130'),
+(296,'JACI F P DA CUNHA','altinoroberto@terra.com.br','1938452123',501,'13510000'),
+(297,'JAMIL TORRECILHA - ME','altinoroberto@terra.com.br','1938763778',1171,'15690000'),
+(298,'MARIA HELENA BRUNELLI - ME','altinoroberto@terra.com.br','1935343431',502,'13505190'),
+(299,'MONTIFREITAS DROGARIAS LTDA - ME','altinoroberto@terra.com.br','1938692239',365,'13270210'),
+(300,'NATANAEL AGUIAR COSTA - EPP','altinoroberto@terra.com.br','1938763778',102,'13280000'),
+(301,'ODONTO SEGA LTDA','piracicaba@odontoclinic.com.br','1934023344',882,'13416585'),
+(302,'OFELIA SARRI MORETE','nfe@supermercadomorete.com.br','1938871250',361,'13185400'),
+(303,'ORAL ODONTOLOGIA - RENE ALEJANDRO LOPERA','altinoroberto@terra.com.br','1934348380',617,'13400120');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(304,'P N AGUIAR & CIA LTDA','drogariaideal@hotmail.com','1938871781',750,'13185230'),
+(305,'PALIMERCIO ANTONIO DE LUCCAS & CIA LTDA','nfe@luccassuper.com.br','1938791901',35,'13190000'),
+(306,'PARDO ODONTOLOGIA LTDA','pardocompras@hotmail.com','1732149877',46,'15015010'),
+(307,'PARDO PREV CONVENIO ODONTOLOGICO LTDA','altinoroberto@terra.com.br','1732149876',62,'15015010'),
+(308,'PHARMAKO FARM MANIP PROD NATURAIS LTDA','altinoroberto@terra.com.br','1938924422',178,'13960000'),
+(309,'PONTIN & SPROESSER LTDA - ME','altinoroberto@terra.com.br','1938791488',654,'13190000'),
+(310,'R ANGELINI FARMACIA - ME','altinoroberto@terra.com.br','1932683436',257,'13060060'),
+(311,'REINALDO JOSE CANOVA','altinoroberto@terra.com.br','1935341288',134,'13500000'),
+(312,'RITA DE CASSIA HERNANDES PARDO - ME','altinoroberto@terra.com.br','1732149877',46,'15015010'),
+(313,'SA E SOUZA COM PROD.FARMACEUTICOS LTDA','altinoroberto@terra.com.br','1938791135',55,'13190000');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(314,'SILVA & TUCHAPSKI DROGARIA LTDA EPP','altinoroberto@terra.com.br','1938833270',411,'13173250'),
+(315,'SILVIA ALBREGARD - ME','altinoroberto@terra.com.br','1935247613',489,'13504000'),
+(316,'TANAKA & GONCALVES DROGARIA LTDA - ME','altinoroberto@terra.com.br','1935043001',479,'13185450'),
+(317,'TAVANO & MORETTO COM.MAT.ODONT.LTDA','odairmoretto@terra.com.br','1932277966',229,'13060074'),
+(318,'TECOM - PREST SERV ODONTOLOGICOS LTDA','altinoroberto@terra.com.br','1938651133',140,'13184290'),
+(319,'UNIMED DE RIO CLARO COOP.TRABALHO MEDICO','leandropavan@unimedrc.com.br','1935222615',1441,'13500240'),
+(320,'UNIMED FARMA COM DE MEDICAM LTDA - EPP','altinoroberto@terra.com.br','1932541316',441,'13093240'),
+(321,'UNIVERSIDADE ESTADUAL DE CAMPINAS','altinoroberto@terra.com.br','1935219004',1026,'13083970'),
+(322,'VIEGAS & VIEGAS LTDA - ME','altinoroberto@terra.com.br','1934618494',1026,'13471310'),
+(323,'VITOR FERNANDES & CIA LTDA - ME','altinoroberto@terra.com.br','1934072494',389,'13465590');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(324,'VOLPI & BUENO DROGARIA LTDA - ME','altinoroberto@terra.com.br','1934682321',853,'13473370'),
+(325,'W & S COM DE MEDICAMENTOS LTDA - EPP','altinoroberto@terra.com.br','1935231771',1039,'13500411'),
+(326,'YAGE COMERCIO DE COSMETICOS LTDA','altinoroberto@terra.com.br','1932342682',261281,'13082490'),
+(327,'ZULMIRA ALESSANDRA DIAS - ME','altinoroberto@terra.com.br','1938763778',1125,'15690000'),
+(328,'COMERCIAL DE GENEROS ALIM. PAULISTA DE MONTE MOR LTDA','nfepaulista@terra.com.br','1938795377',250,'13190000'),
+(329,'ALVIMAR GUEDES - ME','nfe@drogariaguedes.com.br','1935357810',446,'13505100'),
+(330,'SOUZA & PALTRINIERI DROGARIA LTDA - ME','altinoroberto@terra.com.br','1938866866',191,'13280000'),
+(331,'DROGARIA REMANSO LTDA','comprasmaispopular@uol.com.br','1938652510',636,'13188104'),
+(333,'SILVEIRA & CIA LTDA-EPP','nfedrogaria@svicente.com.br', '1934668940', 'AVENIDA AMPELIO GAZETA', '2799', 'ANEXO LUC 05', 'ZONA DE PRODUÇÃO INDUSTRIAL 03', 'NOVA ODESSA', 3533403, 'SP', 1058, 'Brasil', '13460000'),
+(335,'DELISE & PIMENTA COMERCIO DE PRODUTOS FARMACEUTICOS LTDA- ME','farmareal.nfe@gmail.com','1941222800',465,'13183140');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(336,'DROGARIA DOTTA LTDA - EPP','xml.vivafarma@bol.com.br','1934762050',312,'13185230'),
+(337,'DROGARIA REMANSO LTDA','comprasfarmaismontemor@uol.com.br','1938796737',525,'13190000'),
+(338,'DROGARIA REMANSO LTDA','comprasfarmaismirante@uol.com.br','1939093537',2,'13186200'),
+(339,'DROGARIA REMANSO LTDA','comprasfarmaismodelo@uol.com.br','1938652520',193,'13184640'),
+(340,'DROGARIA SUPER POPULAR HORTOLANDIA S.A','notashortolandia3@redesuperpopular.com.br','1939092778',600,'13188160'),
+(341,'DROGARIA SUPER POPULAR HORTOLANDIA S.A','notaseliasfausto@redesuperpopular.com.br','1939092778',113,'13350000'),
+(342,'DROGARIA SUPER POPULAR HORTOLANDIA S.A','notasmontemor2@redesuperpopular.com.br','1939092778',309,'13190000'),
+(343,'DROGARIA SUPER POPULAR HORTOLANDIA S.A','notashortolandia2@redesuperpopular.com.br','1939092778',612,'13184420'),
+(344,'DROGARIA SUPER POPULAR HORTOLANDIA S.A','notaspaulinia@redesuperpopular.com.br','1939092778',35,'13140198'),
+(345,'DROGARIA SUPER POPULAR HORTOLANDIA S.A','notasbraganca@redesuperpopular.com.br','1939092778',17,'12912430');
+
+INSERT INTO customer (id,name,email,phone_number,cityId,zip_code) VALUES
+(346,'DROGARIA SUPER POPULAR HORTOLANDIA S.A','notasbraganca2@resesuperpopular.com.br','1939092778',455,'12900160'),
+(347,'DROGARIA SUPER POPULAR HORTOLANDIA S.A','notashortolandia@redesuperpopular.com.br','1939092778',786,'13184420'),
+(349,'FARMACIA CONCEITO E SAUDE EIRELI - EPP','atendimento@lojaconceitosaude.com.br','1932526166',2066,'13092305'),
+(351,'FREDERICO EDUARDO FELICIO','altinoroberto@terra.com.br','1938651572',140,'13184420'),
+(352,'MARCELO & DORI DROGARIA LTDA - ME','altinoroberto@terra.com.br','1938873411',216,'13185432'),
+(353,'PHARMAKO FARMACIA DE MANIPULAÇÃO','tatiana.vichi@pharmako.com.br','1938924422',114,'13930000'),
+(354,'SAM DROGARIA LTDA - ME','altinoroberto@terra.com.br','1335917246',1907,'11707005'),
+(355,'TONEZE & TONEZE SUMARE LTDA - ME','farmaciapaulista.adm@gmail.com','1938643134',471,'13178110'),
+(356,'TOP FARMA LITORAL LTDA - ME','altinoroberto@terra.com.br','1335942040',11281,'11709005');
