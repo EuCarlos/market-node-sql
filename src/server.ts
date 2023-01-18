b3132d77e9c52dabc944f55bb53cea3a20791b24
Carlos Alves <carlosalves369m@hotmail.com>
2023-01-18 10:51:54 -0300

    :bug: import body-parser and customers routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 require("dotenv").config();
+const bodyParser = require('body-parser')
 
-const productsRoutes = require('./routes/products.routes')
-const citiesRoutes = require('./routes/cities.routes')
+const customerRoutes = require('./routes/customers.routes');
+const productsRoutes = require('./routes/products.routes');
+const citiesRoutes = require('./routes/cities.routes');
 
 const app = express();
+
 const PORT = process.env.PORT || 3333;
 
 app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
+    
+    .use('/api/customers', customerRoutes)
     .use('/api/products', productsRoutes)
     .use('/api/cities', citiesRoutes)
 
